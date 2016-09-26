1f7642d907b20e697de1751dd4d867a759b74451
Christopher Biggs <unixbigot@pobox.com>
2016-09-26 17:21:19 +1000

    Added general-purpose button handling via register_button() function

--- iosetup.lua
+++ iosetup.lua
@@ -47,20 +47,50 @@ function stop_hello()
 end
 
 if config.pin_hello then
-    print("iosetup: diagnostic 'hello world' LED on pin "..config.pin_hello)
+	print("iosetup: diagnostic 'hello world' LED on pin "..config.pin_hello)
 	gpio.mode(config.pin_hello, gpio.OUTPUT)
 	flash_hello(100)
 end
 
 --
--- Configure basic IO pins
+-- Configure basic button support
 --
-if config.pin_button then 
+if config.pin_button then
   print("iosetup: button on pin "..config.pin_button)
-  gpio.mode(config.pin_button, gpio.INT, gpio.PULLUP) 
+  gpio.mode(config.pin_button, gpio.INT, gpio.PULLUP)
 end
 
-if config.pin_ws2812 then 
+button_registry = {}
+
+function button_dispatch(pin, level)
+    print("iosetup: button_dispatch pin="..pin.." level="..level)
+	-- Get the control record for this pin
+	button_info = button_registry[pin];
+	local debounce_delay = (config.button_debounce or 100) * 1000
+
+	-- Ignore duplicate events in rapid succession, mechaninical switches can "bounce" for a few milliseconds
+	if (button_info.last_event and (tmr.now() - button_info.last_event) < debounce_delay) then return end
+
+	button_info.callback(level)
+end
+
+function register_button(pin, trigger, callback)
+    print("iosetup: register_button pin="..pin.." trigger="..trigger)
+	if callback then
+		-- Set up delivery of button events
+		button_registry[pin] = {["callback"] = callback}
+		gpio.trig(pin, trigger, function(level) button_dispatch(pin, level) end)
+	else
+		-- Cancel delivery of button events
+		gpio.trig(pin, "none")
+		button_registry[pin] = nil
+	end
+end
+
+--
+-- Configure 'neopixel' addressable LED support
+--
+if config.pin_ws2812 then
   print("iosetup: WS2812 LED string on pin"..config.pin_ws2812) -- actually only on pin 4
   ws2812.init()
 end
